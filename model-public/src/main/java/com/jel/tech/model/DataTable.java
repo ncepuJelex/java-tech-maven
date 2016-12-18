0cb074292a158ed4d94090e938e6d67fcacbb65f
ncepuJelex <1170366657@qq.com>
2016-12-18 20:20:35 +0800

    移动datatable到model-public

--- model-public/src/main/java/com/jel/tech/model/DataTable.java
+++ model-public/src/main/java/com/jel/tech/model/DataTable.java
@@ -12,34 +12,64 @@ public class DataTable<T> implements java.io.Serializable {
 
 	private static final long serialVersionUID = 6063089265957963178L;
 	
+	private String callback;
+	private String draw;
+	private Integer iDisplayStart;
+	private Integer iDisplayLength;
+	
 	private Long sEcho = 0L;
-	private Long iTotalRecords = 0L;
-	private Long iTotalDisplayRecords = 0L;
+	private Integer iTotalRecords = 0;
+	private Integer iTotalDisplayRecords = 0;
 	private List<T> aaData = new ArrayList<T>();
 	
+	public String getCallback() {
+		return callback;
+	}
+	public void setCallback(String callback) {
+		this.callback = callback;
+	}
+	public String getDraw() {
+		return draw;
+	}
+	public void setDraw(String draw) {
+		this.draw = draw;
+	}
+	public Integer getiDisplayStart() {
+		return iDisplayStart;
+	}
+	public void setiDisplayStart(Integer iDisplayStart) {
+		this.iDisplayStart = iDisplayStart;
+	}
+	public Integer getiDisplayLength() {
+		return iDisplayLength;
+	}
+	public void setiDisplayLength(Integer iDisplayLength) {
+		this.iDisplayLength = iDisplayLength;
+	}
 	public Long getsEcho() {
 		return sEcho;
 	}
 	public void setsEcho(Long sEcho) {
 		this.sEcho = sEcho;
 	}
-	public Long getiTotalRecords() {
+	public Integer getiTotalRecords() {
 		return iTotalRecords;
 	}
-	public void setiTotalRecords(Long iTotalRecords) {
+	public void setiTotalRecords(Integer iTotalRecords) {
 		this.iTotalRecords = iTotalRecords;
 	}
-	public Long getiTotalDisplayRecords() {
+	public Integer getiTotalDisplayRecords() {
 		return iTotalDisplayRecords;
 	}
-	public void setiTotalDisplayRecords(Long iTotalDisplayRecords) {
+	public void setiTotalDisplayRecords(Integer iTotalDisplayRecords) {
 		this.iTotalDisplayRecords = iTotalDisplayRecords;
 	}
-	public List<T> getAaData() {
+	public List<T> getResultData() {
 		return aaData;
 	}
-	public void setAaData(List<T> aaData) {
-		this.aaData = aaData;
+	public void setResultData(List<T> resultData) {
+		this.aaData = resultData;
 	}
+	
 	
 }
