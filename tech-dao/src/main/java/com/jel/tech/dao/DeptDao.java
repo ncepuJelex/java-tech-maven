18b9fe5ad365010a41d285928b7ba4a8590a7a3a
ncepuJelex <1170366657@qq.com>
2016-12-18 20:12:04 +0800

    增加入参为map的服务以测试datatables请求入参为map的功能

--- tech-dao/src/main/java/com/jel/tech/dao/DeptDao.java
+++ tech-dao/src/main/java/com/jel/tech/dao/DeptDao.java
@@ -1,6 +1,7 @@
 package com.jel.tech.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import com.jel.tech.model.Dept;
 import com.jel.tech.model.QueryVo;
@@ -9,6 +10,7 @@ import com.jel.tech.model.QueryVo;
 public interface DeptDao {
 
 	public List<Dept> queryDepts();
+	public List<Dept> queryDeptByName(Map<String, Object> map);
 	public List<Dept> queryDeptsByKeywords(QueryVo vo);
 	public int queryDeptCount(String keywords);
 	public void saveCollege(Dept dept);
